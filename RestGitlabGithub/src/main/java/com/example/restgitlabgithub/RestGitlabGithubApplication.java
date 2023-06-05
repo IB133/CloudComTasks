95464d02481c8713e26b203d21901bf7782a9a9a
Ivan Bragin <bii.20@uni-dubna.ru>
2023-06-05 10:36:48 +0300

    feat: structure project

---
+++ RestGitlabGithub/src/main/java/com/example/restgitlabgithub/RestGitlabGithubApplication.java
@@ -0,0 +1,12 @@
+package com.example.restgitlabgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGitlabGithubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestGitlabGithubApplication.class, args);
+	}
+
+}
---
+++ RestGitlabGithub/src/test/java/com/example/restgitlabgithub/RestGitlabGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restgitlabgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestGitlabGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
